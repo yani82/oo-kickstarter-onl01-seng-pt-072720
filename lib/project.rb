887d6216ef2f5e74c0004168a4568a7a19467914
Yani Asari <yani.iman1@gmail.com>
2020-08-26 04:33:47 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,16 +1,29 @@
-class Project 
+# class Project 
   
-  attr_reader :backers
+#   attr_reader :backers
   
-  def initialize 
+#   def initialize 
+#     @backers = []
+#     @@all << self 
+#   end
+  
+#   def add_backer()
+#     Backer 
+#   end 
+  
+# end
+  
+class Project
+  attr_accessor :title, :backers
+
+  def initialize(title)
+    @title = title
     @backers = []
-    @@all << self 
   end
   
-  def add_backer()
-    Backer 
-  end 
-  
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+  end
 end
-  
   
